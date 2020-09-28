cdae75a5f5f7c6427c27d61dd30406ad82f0cc36
jyx123 <1658865887@qq.com>
2020-09-28 16:24:55 +0800

    测试

--- mysdk/src/main/java/com/example/mysdk/TestJar.java
+++ mysdk/src/main/java/com/example/mysdk/TestJar.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 public class TestJar {
+    //11111
     public static  void TestJar(Context context,String text){
       Toast.makeText(context,text,Toast.LENGTH_SHORT).show();
     }
