1d13685628b466712c507113a7463de81ae8841a
jyx123 <1658865887@qq.com>
2020-09-28 16:08:36 +0800

    Initial commit

---
+++ mysdk/src/main/java/com/example/mysdk/TestJar.java
@@ -0,0 +1,11 @@
+package com.example.mysdk;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+public class TestJar {
+    public static  void TestJar(Context context,String text){
+      Toast.makeText(context,text,Toast.LENGTH_SHORT).show();
+    }
+}
